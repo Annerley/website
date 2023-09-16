8ac75aeeda9b93b38d645549f76c153a0ca98336
Daria <laputina975@gmail.com>
2023-09-17 00:36:22 +0200

    added selector /about

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
 
@@ -15,7 +20,23 @@ public class MainController {
     @GetMapping("/about")
     public String about(Model model) {
         model.addAttribute("title", "About me");
+
+        //gather list from db
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        model.addAttribute("list", cars);
         return "about";
     }
+
+    @PostMapping("/about")
+    public String blogPostAdd(@RequestParam String creature,  Model model){
+        
+        System.out.println(creature);
+        return "redirect:/about";
+    }
+
 
 }
