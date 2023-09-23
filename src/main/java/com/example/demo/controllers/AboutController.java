af99accc4eaa4d250f40e6dba2a167782cb6ee3b
Daria <laputina975@gmail.com>
2023-09-23 23:13:43 +0200

    upgraded AboutView, AboutController

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Creature;
+import com.example.demo.models.CreatureView;
 import com.example.demo.repo.CreatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,13 @@ import java.util.ArrayList;
 @Controller
 public class AboutController {
 
+    String a = "Living Bones";
+    String b = "Abael Scout";
+
+    ArrayList<Creature> tmp = new ArrayList<Creature>();
+
+    ArrayList<String> cre = new ArrayList<String>();
+
     @Autowired
     private CreatureRepository creatureRepository;
     @GetMapping("/about")
@@ -26,8 +34,23 @@ public class AboutController {
         cars.add("BMW");
         cars.add("Ford");
         cars.add("Mazda");
+        cars.add("Living Bones");
+        cars.add("Abael Scout");
+        cars.add("Ancient Artillery");
+
         model.addAttribute("list", cars);
-        Iterable<Creature> creatures = creatureRepository.findAll();
+
+        //foreach
+
+
+
+
+        Iterable<Creature> creatures = tmp;
+
+
+        System.out.println(creatures.toString());
+        //System.out.println(tmp.toString());
+
         model.addAttribute("creatures", creatures);
         return "about";
     }
@@ -35,7 +58,11 @@ public class AboutController {
     @PostMapping("/about")
     public String blogPostAdd(@RequestParam String creature, Model model){
 
-        System.out.println(creature);
+        //creature = найти id по имени
+        cre.add(creature);
+        tmp.add(creatureRepository.findByName(creature).get(0));
+        //Creature a = creatureRepository.findByName(creature.get(0));
+        System.out.println(creatureRepository.findByName(creature).get(0));
         return "redirect:/about";
     }
 }
--- src/main/java/com/example/demo/repo/CreatureRepository.java
+++ src/main/java/com/example/demo/repo/CreatureRepository.java
@@ -2,8 +2,15 @@ package com.example.demo.repo;
 
 import com.example.demo.models.Article;
 import com.example.demo.models.Creature;
+import com.example.demo.models.CreatureView;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
 
 public interface CreatureRepository extends CrudRepository<Creature, Long> {
-    
+    @Query("select b from Creature b where b.name = :name")
+    ArrayList<Creature> findByName(@Param("name") String name);
+
 }
